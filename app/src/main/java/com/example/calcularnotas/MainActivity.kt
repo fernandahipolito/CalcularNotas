87be3067e954b2186c0e7071d2c59362990cb2f0
fernandahipolito <fernanda_hipolito@hotmail.com>
2021-04-01 11:17:13 -0300

    App em funcionamento

--- app/src/main/java/com/example/calcularnotas/MainActivity.kt
+++ app/src/main/java/com/example/calcularnotas/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.calcularnotas
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoCalcular: Button = btn_calcular
+        val resultado: TextView = txt_resultado
+
+        botaoCalcular.setOnClickListener{
+
+            val nota1: Int = Integer.parseInt(edt_nota1.text.toString())
+            val nota2: Int = Integer.parseInt(edt_nota2.text.toString())
+            val media: Int = (nota1+nota2)/2
+            val faltas: Int = Integer.parseInt(edt_faltas.text.toString())
+
+            if(media>=7 && faltas<=10){
+                resultado.setText("O aluno foi aprovado!! \n Média: $media \n  Número de Faltas: $faltas \n ")
+                resultado.setTextColor(Color.WHITE)
+            }else{
+                resultado.setText("O aluno foi reprovado!! \n Média: $media \n  Número de Faltas: $faltas \n ")
+                resultado.setTextColor(Color.YELLOW)
+            }
+        }
     }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Toast.makeText(this,
+        "Obrigada por usar o nosso app!! \n Volte sempre!!",
+        Toast.LENGTH_SHORT).show()
+    }
+
 }
